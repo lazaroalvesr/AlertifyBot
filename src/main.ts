81737242207bd9e9390cfb9158018f2a386a3989
lazaro <lazaroalves12355@gmail.com>
2025-01-18 19:12:12 -0300

    v2

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (typeof global.crypto === 'undefined') {
+    global.crypto = require('crypto');
+  }
+
   await app.listen(3000);
 }
 bootstrap();
